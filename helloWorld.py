b9af2e3522ee95b1410e13a3e7ed7f99b7666cb2
Albinohat <albinohat@gmail.com>
2015-03-14 15:45:26 -0400

    a;sdlfkjsa;dflkja

--- helloWorld.py
+++ helloWorld.py
@@ -2,7 +2,7 @@
 ##
 ## Author(s): Alice & Bob
 
-## I'm in a sad mood, let me talk about it. - Bob
-print "I am a sad line. :("
+## I'm in a happy mood, let me talk about it. - Alice
+print "I am a happy line! :)"
 
 print "Hello, world!"
